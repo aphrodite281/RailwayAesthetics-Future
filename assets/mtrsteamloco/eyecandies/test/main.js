c4615c4e8527cabda4d990aed06c6b7ac6de20e3
aphrodite281 <Aphrodite520@outlook.com>
2024-10-07 15:40:52 +0800

    push

--- assets/mtrsteamloco/eyecandies/test/main.js
+++ assets/mtrsteamloco/eyecandies/test/main.js
@@ -2,51 +2,14 @@ const MCU = MinecraftClient;
 const MCD = MTRClientData;
 const MCR = MTRRailwayData;
 
+const abc = ModelManager.loadRawModel(Resources.manager(), Resources.id("mtrsteamloco:eyecandies/test/1.obj"), null);
+abc.applyUVMirror(false, true);
+const def = ModelManager.uploadVertArrays(abc);
+
 function create(ctx, state, entity) {
 
 }
 
 function render(ctx, state, entity) {
-    ctx.clearDebugInfo();
-    MCD.SCHEDULES_FOR_PLATFORM.forEach((key, value) => {
-        if (key == entity.platformId) {
-            let sches1 = value;
-            let sches = sches1.toArray();
-            sches.sort((a, b) => {
-                return a.arrivalMillis - b.arrivalMillis;
-            });
-            let mins = [sches[0]], texs = []
-            ctx.setDebugInfo("sches", sches.length);
-            ctx.setDebugInfo("kk", sches1.size())
-            let n = getNames(sches[0].routeId)
-            ctx.setDebugInfo("N", n[0]+","+n[1]+","+n[2])
-        }
-    });
-
-}
-
-function getNames(rtid) {
-    for (let rot of MCD.ROUTES) {
-        if (rot.id == rtid) {
-            let ns = [];
-            pids = rot.platformIds;
-            for (let i = 0; i < 2; i++) {
-                p = pids[i == 0 ? 0 : pids.size() - 1];
-                pid = p.platformId;
-                let st;
-                for (let [key, value] of MCD.DATA_CACHE.platformIdToStation) {
-                    if (key == pid) {
-                        st = value;
-                        break;
-                    }
-                }
-                if (st != null) {
-                    n = st.name;
-                }
-                ns[i] = n;
-            }
-            ns[2] = rot.name;
-            return ns;
-        }
-    }
+    ctx.drawModel(def, null);
 }
