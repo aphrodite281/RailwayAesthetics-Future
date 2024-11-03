155338e39731321dbdad7c60c87e4ebc7a31a43c
aphrodite281 <Aphrodite520@outlook.com>
2024-11-03 17:07:37 +0800

    push

--- assets/mtrsteamloco/eyecandies/stop_signb/main.js
+++ assets/mtrsteamloco/eyecandies/stop_signb/main.js
@@ -1,92 +1,88 @@
 include(Resources.id("mtrsteamloco:library/code/face.js"));
+include(Resources.id("mtrsteamloco:library/code/text_u.js"));
+include(Resources.id("mtrsteamloco:library/code/color_int_base.js"));
 
 //const font0 = Resources.getSystemFont("Noto Serif");
 const font0 = Resources.readFont(Resources.id("mtrsteamloco:library/font/hkh_sxt.ttf"));
 const fontSize = 256;
 const font = font0.deriveFont(Font.PLAIN, fontSize);
 
+const cp = (str) => {return TextU.CP(str)};
+
 function create(ctx, state, entity) {
-    //try{
-    
-        if(entity.data.get("scale") == null) {
-            entity.data.put("scale", 1 + "");
-        }
-        if(entity.data.get("color") == null) {
-            entity.data.put("color", "0x000000");
-        }
-        if(entity.data.get("text") == null) {
-            entity.data.put("text", "default");
-        }
 
-        if(isNaN(parseFloat(entity.data.get("scale")))){
-            state.scale = 1;
-            entity.data.put("scale", 1 + "");
-        }else {
-            state.scale = parseFloat(entity.data.get("scale"));
+    let nu = false;
+
+    pi = (key, mnum) => {
+        let num = parseInt(entity.data.get(key));
+        if (isNaN(num)) {
+            num = mnum;
+            entity.data.put(key, mnum + "");
+            nu = true;
         }
-        if(isNaN(parseInt(entity.data.get("color")))){
-            state.color = 0x000000;
-            entity.data.put("color", "0x000000");
-        }else {
-            state.color = parseInt(entity.data.get("color"));
+        return num;
+    }
+    pf = (key, mnum) => {
+        let num = parseFloat(entity.data.get(key));
+        if (isNaN(num)) {
+            num = mnum;
+            entity.data.put(key, mnum + "");
+            nu = true;
         }
-        state.text = entity.data.get("text");
+        return num;
+    }
+    if (!entity.data.containsKey("text")) {
+        entity.data.put("text", "default");
+        nu = true;
+    }
+    state.text = entity.data.get("text");
 
-        newFace(ctx, state, state.text);
-        changeScale(state.scale, state);
-        changeString(state.text, state);
+    state.scale = pf("scale", 1);
+    state.color = pi("color", 0);
 
-        entity.sendUpdateC2S();
-        
-    //}catch(e) {
+    state.face = neww(ctx, state.text, state.scale, state.color);
 
-    //}
+    if (nu) entity.sendUpdateC2S();
 }
 
 function render(ctx, state, entity) {
-    ctx.setDebugInfo("scale",entity.data.get("scale"))
-    ctx.setDebugInfo(entity.data.containsKey("scale"),1)
-    //try{
-        state.face.tick();
+   state.face.tick();
 
-        let newScale = parseFloat(entity.data.get("scale"));
-        if(state.scale!= newScale && isNaN(newScale) == false) {
-            ctx.setDebugInfo("SH","scale: " + state.scale + " -> " + newScale);
-            state.scale = newScale;
-            changeScale(state.scale, state);
+   let nu = false;
+
+    pi = (key, mnum) => {
+        let num = parseInt(entity.data.get(key));
+        if (isNaN(num)) {
+            num = mnum;
+            entity.data.put(key, mnum + "");
+            nu = true;
         }
-
-        let newColor = parseInt(entity.data.get("color"));
-        if(state.color!= newColor && isNaN(newColor) == false) {
-            ctx.setDebugInfo("CH","color: " + state.color + " -> " + newColor);
-            state.color = newColor;
-            changeString(state.text, state);
+        return num;
+    }
+    pf = (key, mnum) => {
+        let num = parseFloat(entity.data.get(key));
+        if (isNaN(num)) {
+            num = mnum;
+            entity.data.put(key, mnum + "");
+            nu = true;
         }
-
-        let newText = entity.data.get("text");
-        if(newText!= null && state.text!= newText) {
-            ctx.setDebugInfo("NH","text: " + state.text + " -> " + newText);
-            state.text = newText;
-            try {
-                state.face.close();
-            }catch(e) {
-
-            }
-            newFace(ctx, state, state.text);
-            changeString(state.text, state);
-            changeScale(state.scale, state);
-        }
-    //}catch(e) {
-
-    //}
+        return num;
+    }
+    let scale = pf("scale", 1);
+    let color = pi("color", 0);
+    let text = entity.data.get("text");
+    if (scale != state.scale || color != state.color || text != state.text) {
+        state.scale = scale;
+        state.color = color;
+        state.text = text;
+        state.face.close();
+        state.face = neww(ctx, state.text, state.scale, state.color);
+    }
+    if (nu) entity.sendUpdateC2S();
 }
 
 function dispose(ctx, state, entity) {
-    try {
-        state.face.close();
-    }catch(e) {
-
-    }
+    state.face.close();
 }
 
 function getSize(str, font) {
@@ -95,7 +91,8 @@ function getSize(str, font) {
     return [Math.ceil(bounds.getWidth()), Math.ceil(bounds.getHeight())];
 }
 
-function newFace(ctx, state, str) {
+
+function neww(ctx, str, scale, color){
     let size = getSize(str, font);
     let face = new Face({
         ctx: ctx,
@@ -103,41 +100,15 @@ function newFace(ctx, state, str) {
         matrices: [new Matrices()],
         texture: size,
         model: {
-            size: [size[0]/100, size[1]/100],
+            size: [size[0]/100 * scale, size[1]/100 * scale],
             renderType: "exteriortranslucent"
         }
     });
-    state.face = face;
-    state.text = str;
-    state.size = size;
-    state.rmA = state.face.rawModel.copy();
-    state.rmA.sourceLocation = null;
-}
-
-function changeScale(scale, state) {
-    let face = state.face;
-    let rmB = state.rmA.copy();
-    rmB.sourceLocation = null;
-    rmB.applyScale(scale, scale, scale);
-    rmB.replaceAllTexture(face.path);
-    face.rawModel = rmB;
-    face.model.uploadLater(rmB);
-    state.scale = scale;
-}
-
-function changeString(text, state) {
-    let face = state.face;
-    try {
-        face.texture.close();
-    }catch(e) {
-        
-    }
-    face.texture = new GraphicsTexture(state.size[0], state.size[1]);
-    let g = face.texture.graphics;
-    g.setColor(new Color(state.color));
+    let tex = face.texture;
+    let g = tex.graphics;
+    g.setColor(new Color(color));
     g.setFont(font);
-    g.drawString(text, 0, state.size[1]);
-    face.texture.upload();
-    face.path = face.texture.identifier;
-    state.text = text;
+    g.drawString(str, 0, size[1]);
+    tex.upload();
+    return face;
 }
