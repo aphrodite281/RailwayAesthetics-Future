b03922dab83a63e47452dc88dee3d6c638e17999
aphrodite281 <Aphrodite520@outlook.com>
2025-03-02 21:50:01 +0800

    push

--- assets/mtr/test/siding.js
+++
@@ -1,5 +0,0 @@
-function render(ctx, state, train) {
-    ctx.setDebugInfo("id", train.mtrTrain().sidingId.toString());
-    ctx.setDebugInfo("name", train.siding().name);
-    ctx.setDebugInfo("idd", train.siding().id.toString());
-}
